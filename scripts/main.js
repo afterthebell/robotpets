778cd290ae1135f072693780e12f44ad77b7425f
A.S. <gachaabbigail@gmail.com>
2026-05-20 09:49:03 -0400

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -12,8 +12,8 @@ function toggleNavigation() {
 // above is the code for the navbar. yeah. something something its a toggle
 
 // amt of the item, cost of the item, all in lists 
-var orders = [0,0,0, 0];
-var costs = [1499.99, 899.99, 1599.99, 999.99];
+var orders = [0,0,0,0, 0, 0];
+var costs = [1499.99, 899.99, 1599.99, 999.99,799.99, 1799.99];
 
 
 // these functions add/subtract an amt from an index depending on the parameter
@@ -44,12 +44,25 @@ function updateScreen() {
     document.getElementById("rabbitText").innerHTML = orders[1];
     document.getElementById("dogText").innerHTML = orders[2];
     document.getElementById("redpandaText").innerHTML = orders[3];
+    document.getElementById("mouseText").innerHTML = orders[4];
+    document.getElementById("ponyText").innerHTML = orders[5];
+
+    document.getElementById("catTextM").innerHTML = orders[0];
+    document.getElementById("rabbitTextM").innerHTML = orders[1];
+    document.getElementById("dogTextM").innerHTML = orders[2];
+    document.getElementById("redpandaTextM").innerHTML = orders[3];
+    document.getElementById("mouseTextM").innerHTML = orders[4];
+    document.getElementById("ponyTextM").innerHTML = orders[5];
 
 
     document.getElementById("catNumber").innerHTML = orders[0];
     document.getElementById("bunnyNumber").innerHTML = orders[1];
     document.getElementById("dogNumber").innerHTML = orders[2];
     document.getElementById("redpandaNumber").innerHTML = orders[3];
+    document.getElementById("mouseNumber").innerHTML = orders[4];
+    document.getElementById("ponyNumber").innerHTML = orders[5];
+
+
 
     // updates the total price for each individual item
     for(var i = 0; i < orders.length; i ++) {
